92f2f10ef0f58704cdc5bdc54aa8dce22bf6302c
A. Bram Neijt <bneijt@gmail.com>
2012-04-21 18:11:30 +0200

    First state to frames

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Animate DOT graphs together into a movie.
+
+Nothing works yet
+=================
+
+
+Working towards
+dotimate [dotfiles]
+which will create an images directory containing all the frames
+
--- src/main/Main.hs
+++ src/main/Main.hs
@@ -7,6 +7,7 @@ import Data.Text
 import Text.Printf
 -- import Data.GraphViz.Types.Canonical
 import Data.GraphViz.Types.Graph
+import System.Directory
 -- Write PNG image file
 -- Neato to PNG first frame
 -- Add edge to end of nodes
@@ -20,13 +21,19 @@ General flow:
 
 emitFrameFromDotFile :: String -> Int -> IO String
 emitFrameFromDotFile dotFileName frameNumber = do
-    _ <- readProcess "neato" ["-Ln1", "-Tdot", "-oframe.dot", dotFileName] "" -- TODO use exec
-    _ <- readProcess "neato" ["-Ln0", "-Tpng", printf "-oimages/frame%05d.png" frameNumber, "frame.dot"] ""
+    _ <- readProcess "neato" ["-Gmaxiter=5", "-Tdot", "-oframe.dot", dotFileName] ""
+    _ <- readProcess "neato" ["-Gmaxiter=1", "-Tpng", printf "-oframes/frame%05d.png" frameNumber, "frame.dot"] ""
     readProcess "mv" ["frame.dot", dotFileName] ""
 
 emitFramesFromDotFile :: String -> Int -> IO()
 emitFramesFromDotFile dotFileName startFrame = do
     mapM_ (emitFrameFromDotFile dotFileName) [startFrame..startFrame + 10]
+
+emitGraph dotGraph = do
+    writeDotFile "state.dot" dotGraph
+    emitFramesFromDotFile "state.dot" 1
+    readDotFile "state.dot" :: IO (DotGraph String)
+
 {-
 addNodes :: oldGraph -> newGraph -> combinedGraph
 prune :: oldGraph -> newGraph -> prunedGraph
@@ -42,8 +49,18 @@ do:
     loadGraph
 -}
 
-main = do
-    args <- getArgs
-    df <- readDotFile "current.dot" :: IO (DotGraph String)
-    writeDotFile "next.dot" (addEdge (args !! 0) (args !! 1) [] df)
+createFramesBetween startFileName endFileName = do
+    startGraph <- readDotFile startFileName :: IO (DotGraph String)
+    endGraph <- readDotFile endFileName :: IO (DotGraph String)
+--    emitGraph 
+--    writeDotFile "next.dot" (addEdge (args !! 0) (args !! 1) [] df)
     emitFrameFromDotFile "next.dot" 10
+    
+
+main = do
+    createDirectoryIfMissing False "frames"
+    startGraph <- readDotFile "some.dot" :: IO (DotGraph String)
+    graph <- emitGraph startGraph
+    putStrLn "done"
+--    foldl nextDotgraph "initial.dot" args
+
