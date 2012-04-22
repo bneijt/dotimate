ca63a91b9671b4a4d120c554faf546d10460bfa3
A. Bram Neijt <bneijt@gmail.com>
2012-04-22 13:48:13 +0200

    Union on nodes

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -5,10 +5,11 @@ import Data.GraphViz.Commands.IO
 import Data.GraphViz.Types
 import Data.Text
 import Text.Printf
--- import Data.GraphViz.Types.Canonical
-import Data.GraphViz.Types.Graph
+import Data.GraphViz.Types.Canonical
+-- import Data.GraphViz.Types.Graph
 import System.Directory
 import Control.Monad
+import Data.List
 -- Write PNG image file
 -- Neato to PNG first frame
 -- Add edge to end of nodes
@@ -32,7 +33,6 @@ emitFrameFromDotFile :: String -> Int -> IO ()
 emitFrameFromDotFile dotFileName frameNumber = do
     _ <- readProcess "neato" ["-Gmaxiter=5", "-Tdot", "-oframe.dot", dotFileName] "" -- TODO exec
     _ <- readProcess "neato" ["-Gmaxiter=1", "-Tpng", printf "-oframes/frame%05d.png" frameNumber, "frame.dot"] ""
-    putStrLn (printf "-oframes/frame%05d.png" frameNumber)
     System.Directory.renameFile "frame.dot" dotFileName
 
 emitFramesFromDotFile :: String -> Int -> IO()
@@ -45,16 +45,33 @@ emitGraph graph frameNumber = do
     emitFramesFromDotFile "state.dot" frameNumber
     readDotFile "state.dot" :: IO (DotGraph String)
 
+nodesOf graph = nodeStmts gs where gs = graphStatements graph
+
+edgesOf graph = edgeStmts gs where gs = graphStatements graph
+
+equalEndpoints :: DotEdge String -> DotEdge String -> Bool
+equalEndpoints edgeA edgeB = (fromNode edgeA == fromNode edgeB) && (toNode edgeA == toNode edgeB)
+
+creatUnionGraph graphA graphB = do
+    let nodesA = nodesOf graphA
+    let nodesB = nodesOf graphB
+    let allNodes = union nodesA nodesB
+    let edgesA = edgesOf graphA
+    let edgesB = edgesOf graphB
+    let allEdges = unionBy equalEndpoints edgesA edgesB
+    graphA { graphStatements = (graphStatements graphA){nodeStmts = allNodes, edgeStmts = allEdges} }
+
+removeNodesAndEdgesNotIn smallGraph possiblyToLargeGraph = possiblyToLargeGraph
 
 emitFramesBetween :: (DotGraph String, Int) -> (DotGraph String, Int) -> IO (DotGraph String, Int)
 emitFramesBetween (currentGraph, aFrameNumber) (nextGraph, bFrameNumber) = do
     afterEmit <- emitGraph currentGraph aFrameNumber
     --Add nodes and edges to the graph
-    let unionGraph = afterEmit
+    let unionGraph = creatUnionGraph afterEmit nextGraph
     afterUnionEmit <- emitGraph unionGraph (aFrameNumber + 10)
     --Remove nodes and egdges from the union
-    let endGraph = afterUnionEmit
-    lastGraph <- emitGraph endGraph (aFrameNumber + 20)
+    let prunedGraph = removeNodesAndEdgesNotIn nextGraph afterUnionEmit
+    lastGraph <- emitGraph prunedGraph (aFrameNumber + 20)
     return (lastGraph, bFrameNumber)
     
 
